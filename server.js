70c664454f5a0d7658d0afcfd9a0b908b2285045
dac <sagarashtekar0007@gmail.com>
2023-01-20 17:04:29 +0530

    done

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname,'./public','./index.html'))
+);
+
+app.listen(3000,()=>
+console.log("Server running at port 3000")
+)
